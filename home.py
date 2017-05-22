9a9377bc68ac77e9c58e6864e63660bab04d5336
vkbinfo <vkb.info@gmail.com>
2017-05-22 11:45:18 +0530

    Added one entity Model through Google DataStore.

--- home.py
+++ home.py
@@ -2,9 +2,15 @@ import os
 import webapp2
 import jinja2
 import re
+from google.appengine.ext import db
 
 template_path=os.path.join(os.path.dirname(__file__),"template")
 JINGA_ENVIRONMENT=jinja2.Environment(loader=jinja2.FileSystemLoader(template_path),autoescape=True)
+
+class Art(db.Model):
+    title=db.StringProperty(required=True)
+    art=db.TextProperty(required=True)
+    time=db.DateTimeProperty(auto_now_add=True)
 
 class handler(webapp2.RequestHandler):
     def write(self,*a):
@@ -18,6 +24,29 @@ class handler(webapp2.RequestHandler):
         self.write(self.render_string(template, **params))
 
 class MainPage(handler):
+    def render_page(self,art='',title='',error=''):
+        self.render("front.html",art=art,title=title,error=error)
+
     def get(self):
-        items=self.request.get_all("items")
-        self.render("mainpage.html",items=items)
+        self.render_page()
+
+    def post(self):
+        title=self.request.get("title")
+        art = self.request.get("art")
+
+        if title and not art:
+            self.render_page(title=title,error="You Should also input the art also.")
+
+        elif not title and art:
+            self.render_page(art=art,error="you must input title also.")
+
+        elif not title and not art:
+            self.render_page(error="you must input art and title")
+
+        else:
+            self.write("Thanks")
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
